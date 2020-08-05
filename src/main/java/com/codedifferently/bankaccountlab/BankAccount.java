faacdf8abd3b59924cedfaac7b120753271fb79c
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-08-05 10:16:39 -0400

    MY MESSAGE HERE

--- src/main/java/com/codedifferently/bankaccountlab/BankAccount.java
+++ src/main/java/com/codedifferently/bankaccountlab/BankAccount.java
@@ -1,4 +1,34 @@
 package com.codedifferently.bankaccountlab;
 
-public class BankAccount {
+public abstract class BankAccount 
+{   
+    private Integer pin;
+    private Integer accountNumber;
+    private double balance = 0;
+    private double fees;
+
+    public BankAccount(Integer pin, Integer accountNumber, double balance, double fees)
+    {
+        this.pin = pin;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.fees = fees;
+    }
+    public Integer getPin()
+    {
+        return this.pin;
+    }
+    public Integer getAccountNumber()
+    {
+        return this.accountNumber;
+    }
+    public double getBalance()
+    {
+        return this.balance;
+    }
+    public double getFees()
+    {
+        return this.fees;
+    }
 }
+
--- src/test/java/com/codedifferently/bankaccountlab/BankAccountTest.java
+++ src/test/java/com/codedifferently/bankaccountlab/BankAccountTest.java
@@ -1,4 +1,18 @@
 package com.codedifferently.bankaccountlab;
 
-public class BankAccountTest {
+import org.junit.Test;
+//import org.junit.Assert;
+public class BankAccountTest
+{   
+    @Test
+    public void constructorTest()
+    {
+        //Given
+        String pin = "0423";
+        Integer accountNumber = 190845643;
+        double balance = 0;
+        double fees = 10.00;
+
+        
+    }
 }
